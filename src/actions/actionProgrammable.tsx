0eb23f187abbd54b05b4ff6a62184c40161f8f8e
huan9huan <hhhust@gmail.com>
2022-08-23 11:59:59 +0800

    Programmable 0.2.2 selector (#9)
    * click to select
    
    * selector

---
+++ src/actions/actionProgrammable.tsx
@@ -0,0 +1,102 @@
+import { selectGroupsForSelectedElements } from "../groups";
+import { changeProperty, getFormValue } from "./actionProperties";
+import { register } from "./register";
+import { newElementWith } from "../element/mutateElement";
+import { ExcalidrawElement } from "../element/types";
+import { getNonDeletedElements, isTextElement } from "../element";
+import { getSelectedElements } from "../scene";
+import { t } from "../i18n";
+
+export const actionChangeClass = register({
+  name: "changeClass",
+  trackEvent: false,
+  perform: (elements, appState, value) => {
+    if (value.type === "update") {
+      return {
+        elements: changeProperty(
+          elements,
+          appState,
+          (el) =>
+            newElementWith(el, {
+              className: value.value,
+            }),
+          true,
+        ),
+        appState: { ...appState, currentItemClassName: value.value },
+        commitToHistory: true,
+      };
+    } else if (value.type === "select") {
+      const selectedElementIds = elements.reduce(
+        (map: Record<ExcalidrawElement["id"], true>, element) => {
+          if (
+            !element.isDeleted &&
+            !(isTextElement(element) && element.containerId) &&
+            !element.locked &&
+            element.className.split(" ").includes(value.value)
+          ) {
+            map[element.id] = true;
+          }
+          return map;
+        },
+        {},
+      );
+
+      return {
+        appState: selectGroupsForSelectedElements(
+          {
+            ...appState,
+            selectedLinearElement: null,
+            editingGroupId: null,
+            selectedElementIds,
+          },
+          getNonDeletedElements(elements),
+        ),
+        commitToHistory: false,
+      };
+    }
+    return { elements, appState, commitToHistory: false };
+  },
+
+  PanelComponent: ({ elements, appState, updateData }) => (
+    <label className="control-label">
+      {t("labels.class") || "class"}
+      {getSelectedElements(elements, appState).length === 1 ? (
+        <input
+          type="text"
+          onChange={(event) =>
+            updateData({ type: "update", value: event.target.value })
+          }
+          value={
+            getFormValue(
+              elements,
+              appState,
+              (element) => element.className,
+              appState.currentItemClassName,
+            ) ?? undefined
+          }
+        />
+      ) : (
+        <div>
+          {elements
+            .filter((e) => e.className)
+            .map((e) => e.className.split(" "))
+            .reduce((all, classes) => all.concat(classes), [] as string[])
+            .reduce(
+              (uniqs, clz) =>
+                uniqs.includes(clz) ? uniqs : uniqs.concat([clz]),
+              [] as string[],
+            )
+            .map((className) => (
+              <span
+                onClick={() => {
+                  updateData({ type: "select", value: className });
+                }}
+              >
+                {className}&nbsp;
+              </span>
+            ))}
+        </div>
+      )}
+    </label>
+  ),
+});
